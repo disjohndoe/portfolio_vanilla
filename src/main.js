f91cd09660e5ebe1507d06d8e55d4386b8d425da
hmatosevic <hmatosevic007@yahoo.com>
2023-09-18 11:38:56 +0200

    Work on the nav and 1st section

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
-import '../styles/modern-normaliye.css';
+import '../styles/modern-normalize.css';
 import '../styles/style.css';
 import '../styles/components/header.css';
+import '../styles/components/hero.css';
 import '../styles/utils.css';
-
